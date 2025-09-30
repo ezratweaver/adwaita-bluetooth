8746dccbe9884e19eaae1bc5ff9cc7996ce27862
ezratweaver <ezratweaver@gmail.com>
2025-09-30 09:34:45 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake for adw-bluetooth applet";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.git
+          (pkgs.python313.withPackages (
+            ps: with ps; [
+              pygobject3
+            ]
+          ))
+        ];
+      };
+    };
+}
