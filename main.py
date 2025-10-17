46614b97cc3d4e0fc1522a950bd5e96648331d34
ezratweaver <ezratweaver@gmail.com>
2025-10-17 13:53:16 -0400

    Remove python

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from adw-bluetooth!")
-
-
-if __name__ == "__main__":
-    main()
--- src/adw_bluetooth/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-from window import AdwBluetooth
-
-
-if __name__ == "__main__":
-    app = AdwBluetooth()
-    app.run(sys.argv)
