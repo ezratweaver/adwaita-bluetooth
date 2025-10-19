88f6a31c4ff5175ca13bc8d6d0b927d96d201ccc
ezratweaver <ezratweaver@gmail.com>
2025-10-19 16:20:33 -0400

    Add device contructor

--- src/bluetooth/device.ts
+++ src/bluetooth/device.ts
@@ -1,13 +1,60 @@
+import Gio from "gi://Gio?version=2.0";
+import { BLUEZ_SERVICE } from "./bluetooth";
+
 export const DEVICE_INTERFACE = "org.bluez.Device1";
 
-export interface Device {
-    address: string;
+interface DeviceProps {
     devicePath: string;
-    alias: string;
-    blocked: boolean;
-    bonded: boolean;
-    connected: boolean;
-    name: string;
-    paired: boolean;
-    trusted: boolean;
+    systemBus: Gio.DBusConnection;
+}
+
+export class Device {
+    private systemBus: Gio.DBusConnection;
+    private devicePath: string;
+    private deviceProxy: Gio.DBusProxy;
+
+    public address: string;
+    public alias: string;
+    public blocked: boolean;
+    public bonded: boolean;
+    public connected: boolean;
+    public name: string;
+    public paired: boolean;
+    public trusted: boolean;
+
+    constructor(props: DeviceProps) {
+        this.systemBus = props.systemBus;
+        this.devicePath = props.devicePath;
+
+        this.deviceProxy = Gio.DBusProxy.new_sync(
+            this.systemBus,
+            Gio.DBusProxyFlags.NONE,
+            null,
+            BLUEZ_SERVICE,
+            this.devicePath,
+            DEVICE_INTERFACE,
+            null,
+        );
+
+        const unpackProperty = <T>(prop: string): T => {
+            const value = this.deviceProxy
+                .get_cached_property(prop)
+                ?.deep_unpack() as T | undefined;
+
+            if (value === undefined) {
+                throw new Error(`Missing device property: ${prop}`);
+            }
+
+            return value;
+        };
+
+        this.address = unpackProperty<string>("Address");
+        this.alias = unpackProperty<string>("Alias");
+        this.blocked = unpackProperty<boolean>("Blocked");
+        this.bonded = unpackProperty<boolean>("Bonded");
+        this.connected = unpackProperty<boolean>("Connected");
+        this.name = unpackProperty<string>("Name");
+        this.paired = unpackProperty<boolean>("Paired");
+        this.trusted = unpackProperty<boolean>("Trusted");
+    }
 }
