c007564f669bd27f109ec2cdd8829748954879ab
ezratweaver <ezratweaver@gmail.com>
2025-10-21 22:57:26 -0400

    Add properties for device details

--- src/device-details-window.ts
+++ src/device-details-window.ts
@@ -5,13 +5,16 @@ import { Device } from "./bluetooth/device.js";
 
 export class DeviceDetailsWindow extends Adw.Window {
     private device: Device;
+    private _paired_row!: Adw.ActionRow;
+    private _type_row!: Adw.ActionRow;
+    private _address_row!: Adw.ActionRow;
 
     static {
         GObject.registerClass(
             {
                 Template:
                     "resource:///com/eweaver/adw_bluetooth/ui/device-details.ui",
-                InternalChildren: [],
+                InternalChildren: ["paired_row", "type_row", "address_row"],
             },
             this,
         );
@@ -23,5 +26,9 @@ export class DeviceDetailsWindow extends Adw.Window {
         });
 
         this.device = device;
+
+        this._paired_row.set_subtitle(device.paired ? "Yes" : "No");
+        this._type_row.set_subtitle("N/A");
+        this._address_row.set_subtitle(device.address);
     }
 }
