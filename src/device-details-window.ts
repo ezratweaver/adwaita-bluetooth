d693590315bdf65c3a0eaf3ec65fcabce0b7cc31
ezratweaver <ezratweaver@gmail.com>
2025-10-22 15:05:17 -0400

    Add forget device button

--- src/device-details-window.ts
+++ src/device-details-window.ts
@@ -2,6 +2,7 @@ import Adw from "gi://Adw";
 import GObject from "gi://GObject";
 import Gtk from "gi://Gtk?version=4.0";
 import { Device } from "./bluetooth/device.js";
+import GLib from "gi://GLib?version=2.0";
 
 export class DeviceDetailsWindow extends Adw.Window {
     private device: Device;
@@ -9,13 +10,20 @@ export class DeviceDetailsWindow extends Adw.Window {
     private _paired_row!: Adw.ActionRow;
     private _type_row!: Adw.ActionRow;
     private _address_row!: Adw.ActionRow;
+    private _forget_button!: Adw.ButtonRow;
 
     static {
         GObject.registerClass(
             {
                 Template:
                     "resource:///com/eweaver/adw_bluetooth/ui/device-details.ui",
-                InternalChildren: ["connection_switch", "paired_row", "type_row", "address_row"],
+                InternalChildren: [
+                    "connection_switch",
+                    "paired_row",
+                    "type_row",
+                    "address_row",
+                    "forget_button",
+                ],
             },
             this,
         );
